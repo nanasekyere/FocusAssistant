fdebb1b7004e6d094dd16c3bbd9d9ba05d315976
nanasekyere <aaronny13@icloud.com>
2025-10-06 15:29:40 +0100

    Initial Commit

---
+++ FocusAssistant/FocusAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAssistantApp.swift
+//  FocusAssistant
+//
+//  Created by Nana Sekyere on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
