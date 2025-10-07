eb959a4a10c0d6a802ffba6c7d5226b85f101f33
nanasekyere <aaronny13@icloud.com>
2025-10-07 13:43:44 +0100

    Added unit test target

---
+++ FocusAssistantTests/FocusAssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  FocusAssistantTests.swift
+//  FocusAssistantTests
+//
+//  Created by Nana Sekyere on 07/10/2025.
+//
+
+import Testing
+
+struct FocusAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
