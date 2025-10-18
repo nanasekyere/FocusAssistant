b54d234d36509f9633af0ae7b2d7fa30241094be
nanasekyere <aaronny13@icloud.com>
2025-10-18 16:09:15 +0100

    Added more tabs

---
+++ FocusAssistant/Screens/Tasks/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  FocusAssistant
+//
+//  Created by Nana Sekyere on 18/10/2025.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
